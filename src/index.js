e1b8969d063f840ddec82306b780eafa18dda280
ibifaa <ibifaaibisakiaruoture@gmail.com>
2023-06-30 09:37:33 +0100

    Change Available Times State to a useReducer
    Created a component called MyContext in which availableTimes useReducer
    was create, and a function updateTimes was passed as
    parameter that will be performed on the state, to get a new state
    to get a new state and initialTimes was also passed
    as the initial value of the availableTimes. availableTimes
    and dispatch was now passed to other components as children.
    On the BookingForm a useEffect was Created
    to handle the dispatch based on the newTime and
    the selected date.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AvailableTimesContext } from './Components/MyContext';
-import { MyContext } from './Components/MyContext'
+import MyContext from './Components/MyContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
